02258a3038fb08570934be6e8d5a85a548991ad7
Amir <iranmanesh@gmail.com>
2023-10-13 12:31:20 -0700

    add new tab named plot correlation to app.py

--- app.py
+++ app.py
@@ -3,9 +3,98 @@ from transformers import pipeline
 import requests
 from tempfile import NamedTemporaryFile
 from PIL import Image
+import pandas as pd
+import numpy as np
 
+# Data Visualization
+# import plotly.express as px
+import plotly.graph_objs as go
+# import plotly.subplots as sp
+from plotly.subplots import make_subplots
+import plotly.figure_factory as ff
+# from IPython.display import display
+# from plotly.offline import init_notebook_mode
+# init_notebook_mode(connected=True)
 
-home_tab, module1_tab, module2_tab = st.tabs(["home", "Image Captioning", "Description to Image"])
+plotly_template = 'simple_white'
+
+def plot_correlation(df):
+    '''
+    This function is resposible to plot a correlation map among features in the dataset
+    '''
+    corr = np.round(df.corr(), 2)
+    mask = np.triu(np.ones_like(corr, dtype = bool))
+    c_mask = np.where(~mask, corr, 100)
+
+    c = []
+    for i in c_mask.tolist()[1:]:
+        c.append([x for x in i if x != 100])
+    
+    fig = ff.create_annotated_heatmap(z=c[::-1],
+                                      x=corr.index.tolist()[:-1],
+                                      y=corr.columns.tolist()[1:][::-1],
+                                      colorscale = 'bluyl')
+
+    fig.update_layout(title = {'text': '<b>Feature Correlation <br> <sup>Heatmap</sup></b>'},
+                      height = 650, width = 650,
+                      margin = dict(t=210, l = 80),
+                      template = 'simple_white',
+                      yaxis = dict(autorange = 'reversed'))
+
+    fig.add_trace(go.Heatmap(z = c[::-1],
+                             colorscale = 'bluyl',
+                             showscale = True,
+                             visible = False))
+    fig.data[1].visible = True
+
+    return fig
+
+def plot_histogram_matrix(df):
+    
+    '''
+    This function identifies all continuous features within the dataset and plots
+    a matrix of histograms for each attribute
+    '''
+    
+    continuous_features = []
+    for feat in df.columns:
+        if df[feat].nunique() > 2:
+            continuous_features.append(feat)
+    num_cols = 2
+    num_rows = (len(continuous_features) + 1) // num_cols
+
+    fig = make_subplots(rows=num_rows, cols=num_cols)
+
+    for i, feature in enumerate(continuous_features):
+        row = i // num_cols + 1
+        col = i % num_cols + 1
+
+        fig.add_trace(
+            go.Histogram(
+                x=df[feature],
+                name=feature
+            ),
+            row=row,
+            col=col
+        )
+
+        fig.update_xaxes(title_text=feature, row=row, col=col)
+        fig.update_yaxes(title_text='Frequency', row=row, col=col)
+        fig.update_layout(
+            title=f'<b>Histogram Matrix<br> <sup> Continuous Features</sup></b>',
+            showlegend=False
+        )
+
+    fig.update_layout(
+        height=350 * num_rows,
+        width=1000,
+        margin=dict(t=100, l=80),
+        template= plotly_template
+    )
+
+    return fig
+
+home_tab, module1_tab, module2_tab,  plot_correlation_tab = st.tabs(["home", "Image Captioning", "Description to Image", "Plot Correlation"])
 with module1_tab:
     # pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
     # st.title("Image Captioning App")
@@ -62,3 +151,19 @@ with module2_tab:
     from PIL import Image
     image = Image.open(io.BytesIO(image_bytes))
     st.image(image, caption="Selected Image", use_column_width=True)
+
+with plot_correlation_tab:
+    uploaded_file = st.file_uploader("Import a CSV file", type=["csv"])
+    if uploaded_file is not None:
+        with NamedTemporaryFile(dir='.', suffix='.jpeg') as f:
+            f.write(uploaded_file.getbuffer())
+        try:
+            df = pd.read_csv(uploaded_file)
+            st.dataframe(df)
+            st.data_editor(df)
+            st.plotly_chart(plot_correlation(df), theme="streamlit")
+            # st.plotly_chart(plot_histogram_matrix(df), theme="streamlit")
+        except Exception as e:
+            st.text(str(e))
+    
+    
